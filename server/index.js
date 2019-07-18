baac9c89201388444a574aab48dc60444dbc12de
cnsarmi <christiannsarmiento@gmail.com>
2019-07-17 17:17:30 -0700

    Proxy server is running all modules. Need to refactor css

--- server/index.js
+++ server/index.js
@@ -10,5 +10,24 @@ app.all('/listings', function(req, res) {
     apiProxy.web(req, res, {target: serverOne, changeOrigin: true})
 });
 
+app.all('/app1', (req, res) => {
+    // console.log('redirecting to  the app1');
+    proxy.web(req, res, {target: 'http://127.0.0.1:4001', changeOrigin: true})
+  });
+  
+  app.all('/recommendations', (req, res) => {
+    // console.log('redirecting to  the app1');
+    proxy.web(req, res, {target: 'http://127.0.0.1:4001', changeOrigin: true})
+  });
+  
+  app.all('/recommendations/save', (req, res) => {
+    // console.log('redirecting to  the app1');
+    proxy.web(req, res, {target: 'http://127.0.0.1:4001', changeOrigin: true})
+  });
+  
+  app.all('/recommendations/unsave', (req, res) => {
+    // console.log('redirecting to  the app1');
+    proxy.web(req, res, {target: 'http://127.0.0.1:4001', changeOrigin: true})
+  });
 
 app.listen(3001, console.log(`3001 is lisenting!`));
